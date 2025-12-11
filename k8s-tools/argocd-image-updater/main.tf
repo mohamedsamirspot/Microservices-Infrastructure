617c76955c463cbfea2a743490ceb95f684e53ba
Mohamed Samir <71722372+mohamedsamirspot@users.noreply.github.com>
2025-12-11 14:40:09 +0200

    Add argocd image updater

--- k8s-tools/argocd-image-updater/main.tf
+++ k8s-tools/argocd-image-updater/main.tf
@@ -16,3 +16,42 @@ resource "helm_release" "argocd-image-updater" {
   #   value = "LoadBalancer"
   # }
 }
+
+
+resource "helm_release" "argocd-image-updater" {
+  name             = "argocd-image-updater"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-image-updater"
+  version          = var.argocd_chart_version
+  namespace        = "argocd"
+  create_namespace = true
+
+  # values = [
+  #   file("${path.module}/values-argocd-image-updater.yaml")
+  # ]
+
+  # Example of inline value override
+  # set {
+  #   name  = "server.service.type"
+  #   value = "LoadBalancer"
+  # }
+}
+
+data "aws_secretsmanager_secret_version" "github_token" {
+  secret_id = "terraform/github-token"
+}
+
+resource "kubectl_manifest" "gh_auth_token" {
+  yaml_body = <<-YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: git-creds
+  namespace: argocd
+type: Opaque
+data:
+  username: "${base64encode("mohamedsamirspot")}"
+  password: "${base64encode(jsondecode(data.aws_secretsmanager_secret_version.github_token.secret_string)["github-token"])}"
+YAML
+  depends_on = [helm_release.argocd-image-updater]
+}
