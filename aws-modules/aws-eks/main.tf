075d27d141549315c6356d03d934effe88b9d7e1
Mohamed Samir <71722372+mohamedsamirspot@users.noreply.github.com>
2025-12-30 16:13:28 +0200

    edit

--- aws-modules/aws-eks/main.tf
+++ aws-modules/aws-eks/main.tf
@@ -129,20 +129,31 @@ module "eks" {
 
   # Cluster access entry
   # To add the current caller identity as an administrator
-  enable_cluster_creator_admin_permissions = true
+  # enable_cluster_creator_admin_permissions = true
   access_entries = {
     # you can use users or roles arns only no groups so you need to put more than one user or just put one role and make all the users you want to assume it so they can have access from one access entry only
-    # mohamed-emary = {
-    #   principal_arn     = "arn:aws:iam::948763340657:user/spot"
-    #   policy_associations = {
-    #     example = {
-    #       policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-    #       access_scope = {
-    #             type = "cluster"
-    #       }
-    #     }
-    #   }
-    # }
+    spot = {
+      principal_arn     = "arn:aws:iam::948763340657:user/spot"
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+                type = "cluster"
+          }
+        }
+      }
+    }
+    github-action-role = {
+      principal_arn     = "arn:aws:iam::948763340657:role/github-action-role"
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+                type = "cluster"
+          }
+        }
+      }
+    }
     # moataznaguib = {
     #   principal_arn     = "arn:aws:iam::948763340657:user/moataznaguib"
     #   policy_associations = {
