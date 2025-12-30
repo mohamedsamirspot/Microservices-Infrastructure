479b9eb38ecf13c1c46eae1201385c488976e3e0
Mohamed Samir <71722372+mohamedsamirspot@users.noreply.github.com>
2025-12-30 14:15:40 +0200

    Refactor the code and remove unneeded terragrunt inputs

--- k8s-tools/stakater-reloader/variables.tf
+++ k8s-tools/stakater-reloader/variables.tf
@@ -1,17 +1,3 @@
-variable "tags" {
-  description = "Map of tags to assign"
-  type        = map(string)
-  default = {
-    env         = "dev"
-    terraform   = "true"
-  }
-}
-
-variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-}
-
 variable "stakater-reloader_chart_version" {
   description = "Version of the stakater-reloader Helm chart"
   type        = string
