f46bdc8b71ed1a64f731c2e26d3274f641e4ff0c
Mohamed Samir <71722372+mohamedsamirspot@users.noreply.github.com>
2025-12-30 14:44:17 +0200

    edit

--- aws-modules/aws-eks/main.tf
+++ aws-modules/aws-eks/main.tf
@@ -69,7 +69,7 @@ module "eks" {
   # EKS Managed Node Group(s)
 
   eks_managed_node_groups = {
-    "${var.cluster_name}"-bootstrapping-node-group = {
+    "${var.cluster_name}-bootstrapping-node-group" = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = var.instance_type
