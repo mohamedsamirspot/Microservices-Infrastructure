a6b013be6511e3b06d6b39a55ee142c93f483af6
Mohamed Samir <mohamed.samir.emary@gmail.com>
2026-01-02 23:29:35 +0200

    edit

--- k8s-tools/sonarqube/main.tf
+++ k8s-tools/sonarqube/main.tf
@@ -93,7 +93,7 @@ resource "helm_release" "sonarqube" {
     templatefile("${path.module}/values-sonarqube.yaml", {
       sonarpassword      = "${base64encode(jsondecode(data.aws_secretsmanager_secret_version.sonarpassword.secret_string)["password"])}"
       monitoringpasscode = module.monitoring_passcode.secret_string
-      endpoint   = module.db.db_instance_endpoint
+      endpoint   = "jdbc:postgresql://${module.db.db_instance_endpoint}:5432/sonarqube"
     })
   ]
 
