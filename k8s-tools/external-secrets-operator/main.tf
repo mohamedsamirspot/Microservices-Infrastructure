27df1aa104afb3b9fd2bbdb7b307543b7ec4c61c
Mohamed Samir <mohamed.samir.emary@gmail.com>
2026-01-12 18:07:32 +0200

    edit

--- k8s-tools/external-secrets-operator/main.tf
+++ k8s-tools/external-secrets-operator/main.tf
@@ -105,7 +105,7 @@ YAML
 module "secrets_manager" {
   source = "terraform-aws-modules/secrets-manager/aws"
   version = "2.1.0"
-  
+
   # Secret
   name             = "microservices-secret-2"
   recovery_window_in_days = 0
