a3761df908826a05d9910f4cfdc90e75178045b2
Mohamed Samir <71722372+mohamedsamirspot@users.noreply.github.com>
2026-02-12 11:37:46 +0200

    Generate random password for grafana admin user

--- k8s-tools/monitoring/grafana/main.tf
+++ k8s-tools/monitoring/grafana/main.tf
@@ -1,27 +1,62 @@
-module "secrets_manager" {
-  source = "terraform-aws-modules/secrets-manager/aws"
-  version = "2.1.0"
+# module "secrets_manager" {
+#   source = "terraform-aws-modules/secrets-manager/aws"
+#   version = "2.1.0"
 
-  # Secret
-  name             = "grafana-admin-password"
-  recovery_window_in_days = 0
-  ignore_secret_changes = true
+#   # Secret
+#   name             = "grafana-admin-password"
+#   recovery_window_in_days = 0
+#   ignore_secret_changes = true
 
-  # Policy
-  create_policy       = false
-  block_public_policy = true
+#   # Policy
+#   create_policy       = false
+#   block_public_policy = true
 
-  # Version
-  create_random_password           = true
-  random_password_length           = 64
-  random_password_override_special = "!@#$%^&*()_+"
+#   # Version
+#   create_random_password           = true
+#   random_password_length           = 64
+#   random_password_override_special = "!@#$%^&*()_+"
 
-  tags = var.tags
+#   tags = var.tags
+# }
+
+# data "aws_secretsmanager_secret_version" "grafana_admin" {
+#   secret_id  = module.secrets_manager.secret_id
+#   version_id = module.secrets_manager.secret_version_id
+# }
+
+resource "kubectl_manifest" "monitoring_namespace" {
+  yaml_body = <<-YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: monitoring
+YAML
 }
 
-data "aws_secretsmanager_secret_version" "grafana_admin" {
-  secret_id  = module.secrets_manager.secret_id
-  version_id = module.secrets_manager.secret_version_id
+# Generate a random password for Grafana admin
+resource "random_password" "grafana_admin_password" {
+  length  = 24
+  special = true
+  upper   = true
+  lower   = true
+  numeric = true
+}
+
+# Create Kubernetes secret for Grafana admin credentials
+resource "kubectl_manifest" "grafana_admin_secret" {
+  yaml_body = <<-YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: grafana-admin-credentials
+  namespace: monitoring
+type: Opaque
+stringData:
+  userKey: admin
+  passwordKey: ${random_password.grafana_admin_password.result}
+YAML
+
+  depends_on = [kubectl_manifest.monitoring_namespace]
 }
 
 resource "helm_release" "grafana" {
@@ -32,11 +67,14 @@ resource "helm_release" "grafana" {
   namespace        = "monitoring"
   create_namespace = true
 
+  # values = [
+  #   templatefile("${path.module}/values-grafana.yaml", {
+  #       grafanaadminpassword      = data.aws_secretsmanager_secret_version.grafana_admin.secret_string
+  #     })
+  # ]
   values = [
-    templatefile("${path.module}/values-grafana.yaml", {
-        grafanaadminpassword      = data.aws_secretsmanager_secret_version.grafana_admin.secret_string
-      })
+    file("${path.module}/values-grafana.yaml")
   ]
-
-  depends_on = [ module.secrets_manager]
+  # depends_on = [ module.secrets_manager]
+  depends_on = [kubectl_manifest.grafana_admin_secret]
 }
