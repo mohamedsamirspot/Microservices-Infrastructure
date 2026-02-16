6484f22a2f0daab5010431f290578ea2af3fa945
Mohamed Samir <mohamed.samir.emary@gmail.com>
2026-02-16 16:58:00 +0200

    edit

--- k8s-tools/envoy-gateway-api/main.tf
+++ k8s-tools/envoy-gateway-api/main.tf
@@ -1,17 +1,16 @@
 # This chart include both the Envoy Gateway and Gateway API crds and you can skip the gateway api crds if you want to install them separately and i think it's better to install them separately if you have more than one gateway implementation in the cluster
 # https://gateway.envoyproxy.io/v1.5/install/install-helm/
 
-resource "helm_release" "envoy-crds" {
-  name             = "envoy-crds"
-  repository       = "oci://docker.io/envoyproxy"
-  chart            = "gateway-crds-helm"
-  version          = var.envoy-crds_chart_version
-  namespace        = "envoy-gateway-api"
-  create_namespace = true
-  values = [
-    file("values-envoy-crds.yaml")
-  ]
+resource "null_resource" "apply_envoy_crds" {
+  provisioner "local-exec" {
+    command = <<EOT
+      helm template eg-crds oci://docker.io/envoyproxy/gateway-crds-helm \
+        --version 1.6.2 --set crds.gatewayAPI.enabled=false --set crds.envoyGateway.enabled=true | \
+      kubectl apply --server-side -f -
+    EOT
+  }
 }
+
 
 # The controller itself
 resource "helm_release" "envoy-gateway-api" {
@@ -21,6 +20,6 @@ resource "helm_release" "envoy-gateway-api" {
   version          = var.envoy-gateway-api_chart_version
   namespace        = "envoy-gateway-api"
   create_namespace = true
-  skip_crds        = false
+  skip_crds        = true
 }
 
--- k8s-tools/envoy-gateway-api/variables.tf
+++ k8s-tools/envoy-gateway-api/variables.tf
@@ -3,9 +3,3 @@ variable "envoy-gateway-api_chart_version" {
   type        = string
   default     = "1.6.2"
 }
-
-variable "envoy-crds_chart_version" {
-  description = "Version of the custome-envoy-crds Helm chart"
-  type        = string
-  default     = "1.6.2"
-}
